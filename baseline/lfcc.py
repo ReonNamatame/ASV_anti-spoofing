41672340e8788892474504386ec166d2eb827d44
ReonNamatame <reon64.1173@gmail.com>
2020-08-18 00:12:47 +0900

    add LFCCs

---
+++ baseline/lfcc.py
@@ -0,0 +1,159 @@
+#import wave
+import numpy as np
+import scipy.signal
+from scipy.io.wavfile import read
+from scipy.fftpack import realtransforms
+
+import matplotlib.pyplot as plt
+
+# Linear Frequency Cepstral Coefficients(LFCCs)
+#beginning of class LFCC
+class LFCC(object):
+
+#public method
+    def __init__(self, wavfile="record.wav"):
+        #%time wave.open(str(wavfile), mode="rb")
+        #%time read(wavfile)
+        self._sr, self._waveform = read(wavfile)
+        
+    def get_audio_signal(self):
+        return self._waveform
+    
+    def get_sampling_rate(self):
+        return self._sr
+    
+    def get_mfcc(self, p=0.97, nchannels=24, nceps=12):
+        
+        # define a convolute preEmphasis filter
+        self._waveform = self._preEmphasis(self._waveform, p)
+        #print(type(self._waveform))
+        
+        # define a hamming window
+        self._nfft = 512
+        hammingWindow = np.hamming(self._nfft)
+        
+        # make a spectrogram
+        spec = self._stft(wave=self._waveform, window=hammingWindow, step=self._nfft//2)
+        # n: fft_bin, d: cycle time
+        freq = np.fft.fftfreq(n=self._nfft, d=1.0/self._sr)[:self._nfft//2]
+
+        """
+        for i, sp in enumerate(spec):
+            plt.plot(freq, sp)
+        plt.show()
+        """
+        """
+        import scipy.io.wavfile as wav
+        import scipy.signal as signal
+        import librosa
+
+        f, t, Zxx = signal.stft(self._waveform, fs=self._sr)
+        #plt.pcolormesh(t, f, , shading='gouraud')
+        plt.ylabel('Frequency [Hz]')
+        plt.xlabel('Time [sec]')
+        plt.pcolormesh(t, f, np.abs(Zxx), shading='gouraud')
+        plt.show()
+
+        #DB = librosa.amplitude_to_db(Zxx, ref=np.max)
+        #lirosa.display.specshow(DB, sr=self._sr, hop_length=self._nfft, x_axis='time', y_axis='log')
+        #plt.colorbar(format='%+2.0fdB')
+        """
+
+        #linearfilterbank
+        df = self._sr / self._nfft # frequency resolution
+        filterbank = self._linearFilterBank(nchannels=nchannels)
+        print(filterbank.shape)
+        """
+        for c in np.arange(0, nchannels):
+            plt.plot(np.arange(0, self._nfft//2) * df, filterbank[c])
+        plt.show()
+        """
+        # apply linearfilterbanks for each vector, then sum all and take log
+        mspec = np.log10(np.dot(spec, filterbank.T))
+        print("mspec:", mspec.shape)
+        
+        # obtain a cepstram by applying discrete cosine transform to log-linear spectrogram
+        cepstram = self._dct(mspec).T
+        
+        # cepstram = (n-dimensional feature, shift), so select the number of feature to use
+        mfccs = cepstram[:nceps]
+
+        return mfccs
+
+    #pre-emphasis filter
+    def _preEmphasis(self, signal, p):
+        #signal := voice_signal, p := coefficient
+        #make FIR filter such that coefficients are (1.0, p)
+        return scipy.signal.lfilter([1.0, -p], 1.0, signal)
+    
+    #convert hz to mel
+    def _hz2mel(self, f):
+        return 1125.0 * np.log(f/700.0 + 1.0)
+    
+    #convert mel to hz
+    def _mel2hz(self, m):
+        return 700.0 * (np.exp(m/1125.0) - 1.0)
+    
+    #Short Time Fourier Transform: STFT
+    def _stft(self, wave, window, step):
+        # wave: waveform (numpy.ndarray)
+        # window: hammingWindow (numpy.ndarray)
+        # step: overlapping length
+        wavelen = wave.shape[0]
+        windowlen = window.shape[0] # windowLength, fft_bin, cripping_size
+        shift = (wavelen - windowlen + step-1) // step + 1
+        print("wavelen: {}, fft_bin: {}, shift: {}".format(wavelen, windowlen, shift))
+
+        #padded_wave = np.zeros(wavelen+step)
+        #padded_wave = np.zeros(windowlen+(shift*step))
+        #padded_wave[:wavelen] = wave # waveform has to be reformed to fit the sliding window 
+        
+        X = np.array([]) # X: spectrogram will have to be (shift, windowlen)
+        for m in range(shift):
+            start = step * m
+            x = np.fft.fft(wave[start:start+windowlen]*window, norm='ortho')[:self._nfft//2]
+            if m == 0:
+                X = np.hstack((X, x))
+            else:
+                X = np.vstack((X, x))
+            """
+            if m == 120:
+                plt.plot(np.arange(0, windowlen), wave[start:start+windowlen]*window)
+                plt.show()
+                plt.plot(np.fft.fftfreq(n=self._nfft, d=1.0/self._sr)[:self._nfft//2], np.abs(x))
+                plt.show()
+            """
+        print(X.shape)
+        return np.abs(X)**2/self._nfft
+    
+    #generate linearfilterbank
+    def _linearFilterBank(self, nchannels=40):
+        freq_min = 0
+        freq_max = self._sr//2
+        linear_centers = np.linspace(freq_min, freq_max, nchannels+2)
+        bins = np.floor((self._nfft+1)*linear_centers/self._sr)
+        #print(bins)
+
+        filterbank = np.zeros((nchannels, self._nfft//2))
+        for m in range(1, nchannels+1):
+            f_m_minus = int(bins[m - 1])    # left
+            f_m = int(bins[m])              # center
+            f_m_plus = int(bins[m + 1])     # right
+
+            for k in range(f_m_minus, f_m):
+                filterbank[m - 1, k] = (k - bins[m - 1]) / (bins[m] - bins[m - 1])
+            for k in range(f_m, f_m_plus):
+                filterbank[m - 1, k] = (bins[m + 1] - k) / (bins[m + 1] - bins[m])
+
+        return filterbank
+    
+    #discrete cosine transform
+    def _dct(self, mspec):
+        return realtransforms.dct(mspec, type=2, norm='ortho', axis=-1)
+    
+#end of class LFCC
+
+if __name__ == "__main__":
+    lfcc = LFCC("utterance3.wav").get_mfcc()
+
+    print(lfcc, lfcc.shape)
---
+++ baseline/mfcc.py
@@ -0,0 +1,98 @@
+import wave
+import numpy as np
+import scipy.signal
+from scipy.fftpack import realtransforms
+
+#beginning of class MFCC
+class MFCC(object):
+    
+#public method
+    def __init__(self, wavfile="record.wav"):
+        self.__waveform = wave.open(str(wavfile), mode="rb")
+        self.__freq = self.__waveform.getframerate()
+    
+    def get_sampliing_freq(self):
+        return self.__freq
+    
+    def get_mfcc(self, p=0.97, nchannels=24, nceps=12):
+        #extract raw data from wavfile
+        __signal = []
+        __signal = self.__waveform.readframes(-1) #read all data
+        __signal = np.frombuffer(__signal, dtype="int16") / float(2**15) #normalize
+
+        #multiply preEmphasis filter
+        __signal = self.__preEmphasis(__signal, p)
+        #hamming window
+        dataSz = len(__signal)
+        hammingWindow = np.hamming(dataSz)
+        __signal = __signal * hammingWindow
+
+        #get spectrum
+        self.__nfft = 2**int(np.log2(dataSz)+1)
+        spec = np.abs(np.fft.fft(__signal, self.__nfft)/(self.__nfft/2))[:self.__nfft//2]
+
+        #melfilterbank
+        #numChannels = 24
+        df = self.__freq / self.__nfft
+        filterbank, fcenters = self.__melFilterBank(self.__freq, self.__nfft, nchannels)
+
+        #apply melfilterbank to each spectrum, then take sum all
+        mspec = []
+        for c in np.arange(0, nchannels):
+            mspec.append(np.log10(sum(spec * filterbank[c])))
+        mspec = np.array(mspec)
+        
+        #get mfcc by discrete cosine form
+        mfcc = self.__dct(mspec, nceps)
+        #print(type(mfcc))
+        return mfcc
+
+#private methods
+    #pre-emphasis filter
+    def __preEmphasis(self, signal, p):
+        #signal := voice_signal, p := coefficient
+        #make FIR filter such that coefficients are (1.0, p)
+        return scipy.signal.lfilter([1.0, -p], 1, signal)
+    
+    #convert hz to mel
+    def __hz2mel(self, f):
+        return 1127.01048 * np.log(f/700.0 + 1.0)
+    
+    #convert mel to hz
+    def __mel2hz(self, m):
+        return 700*(np.exp(m/1127.01048) - 1.0)
+
+    #generate melfilterbank
+    def __melFilterBank(self, fs, nfft, nchannels):
+        #ナイキスト周波数
+        fmax = fs / 2
+        melmax = self.__hz2mel(fmax)
+        nmax = nfft // 2
+        df = fs / nfft
+        dmel = melmax / (nchannels + 1)
+        melcenters = np.arange(1, nchannels + 1) * dmel
+        fcenter = self.__mel2hz(melcenters)
+        indexcenter = np.round(fcenter / df)
+        indexstart = np.hstack((0, indexcenter[0:nchannels - 1]))
+        # 各フィルタの終了位置のインデックス
+        indexstop = np.hstack((indexcenter[1:nchannels], [nmax]))
+        
+        filterbank = np.zeros((nchannels, nmax))
+        for c in np.arange(0, nchannels):
+            # 三角フィルタの左の直線の傾きから点を求める
+            increment= 1.0 / (indexcenter[c] - indexstart[c])
+            for i in np.arange(indexstart[c], indexcenter[c]):
+                filterbank[c, int(i)] = (i - indexstart[c]) * increment
+            # 三角フィルタの右の直線の傾きから点を求める
+            decrement = 1.0 / (indexstop[c] - indexcenter[c])
+            for i in np.arange(indexcenter[c], indexstop[c]):
+                filterbank[c, int(i)] = 1.0 - ((i - indexcenter[c]) * decrement)
+        return filterbank, fcenter
+    
+    #discrete cosine transform
+    def __dct(self, mspec, nceps):
+        ceps = realtransforms.dct(mspec, type=2, norm="ortho", axis=-1)
+        #return lower features by n
+        return ceps[:nceps]
+
+#end of class MFCC
